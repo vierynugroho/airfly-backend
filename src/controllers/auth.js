2969d58084d8bce0152778782370079777f2fd9b
BoboiAzumi <untukherokudeploy@gmail.com>
2024-11-26 20:19:56 +0700

    refactor: authentication

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -36,43 +36,12 @@ export class AuthController {
     try {
       const { firstName, lastName, phone, email, password } = req.body;
 
-      const token = await AuthService.register(
-        firstName,
-        lastName,
-        phone,
-        email,
-        password
-      );
+      await AuthService.register(firstName, lastName, phone, email, password);
 
       return res.json({
         meta: {
           statusCode: 201,
           message: "user has created, let's verify",
-        },
-        data: {
-          //redirect: `/api/v1/auth/verify?token=${token}`,
-          token,
-        },
-      });
-    } catch (e) {
-      next(e);
-    }
-  }
-
-  /**
-   *
-   * @param {import('express').Request} req
-   * @param {import('express').Response} res
-   * @param {import('express').NextFunction} next
-   */
-
-  static async OTP(req, res, next) {
-    try {
-      await AuthService.otp(req.body.token);
-      res.json({
-        meta: {
-          statusCode: 200,
-          message: 'OTP sent',
         },
       });
     } catch (e) {
@@ -89,12 +58,13 @@ export class AuthController {
 
   static async verify(req, res, next) {
     try {
-      const { otp, token } = req.body;
-      await AuthService.verify(otp, token);
+      const { otp, email } = req.body;
+      const verify = await AuthService.verify(otp, email);
       res.json({
         meta: {
           statusCode: 200,
-          message: 'User has been verified',
+          message:
+            verify == 'VERIFIED' ? 'user has been verified' : 'OTP is valid',
         },
       });
     } catch (e) {
