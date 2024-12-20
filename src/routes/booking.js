cbcf95d13be2a5b49865754a48615f0191f16e73
vnonymous_ <61929120+vierynugroho@users.noreply.github.com>
2024-12-21 01:33:58 +0700

    fix(payment): remove validation data on payment

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { UserRole } from '@prisma/client';
-import validation from '../middlewares/validator.js';
 import { authorization } from '../middlewares/authorization.js';
 import { BookingController } from '../controllers/booking.js';
 import { bookingValidationSchema } from '../utils/validationSchema.js';
@@ -10,8 +9,7 @@ export const bookingRoute = Router();
 bookingRoute
   .route('/')
   .post(
-    authorization([UserRole.BUYER]), 
-    validation(bookingValidationSchema),
+    authorization([UserRole.BUYER]),
     BookingController.createBooking)
   .get(
     authorization([UserRole.BUYER, UserRole.ADMIN]),
